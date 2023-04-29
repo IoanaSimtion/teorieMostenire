bedd84e2ab8a487da0bf5764fa6032fdeafd3c57
Ioana Simtion <ionitza6@gmail.com>
2023-04-29 17:02:21 +0300

    added baza si derivate

---
+++ teorieMostenire/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using teorieMostenire;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vehicul vehicul = new Vehicul();
+
+            // vehicul.Pret = 220;
+            // vehicul.Model = "MAKD";
+            // vehicul.Marca = "adaa";
+
+            // /*Console.WriteLine(vehicul.Pret);
+            // Console.WriteLine(vehicul.Marca);
+            // Console.WriteLine(vehicul.Model);*/
+
+            // Avion avion = new Avion();
+
+            // avion.Pret = 235211;
+            // avion.Model = "svaav";
+            // avion.Marca = "AVNVVS";
+            // avion.ModelTurbina = "xxx";
+            // avion.ModelAripi = "abc";
+            // avion.TipAvion = "zva";
+
+            //Avion avion2=new Avion("svabav", "basVVS", 567211, "df", "yyy", "maoer");
+
+            //Console.WriteLine(avion.Pret);
+            //Console.WriteLine(avion2.Pret);
+            //Console.WriteLine(avion.ModelAripi);
+            //Console.WriteLine(avion2.ModelAripi);
+            //Console.WriteLine(avion.Marca);
+            //Console.WriteLine(avion2.Marca);
+            //Console.WriteLine(avion.TipAvion);
+            //Console.WriteLine(avion2.TipAvion);
+            //Console.WriteLine(avion.Model);
+            //Console.WriteLine(avion2.Model);
+            //Console.WriteLine(avion.ModelTurbina);
+            //Console.WriteLine(avion2.ModelTurbina);
+
+            Avion avion = new Avion("MCOADM", "nvs", 1444, "abc", "xxx", "navn");
+            Console.WriteLine(avion.Pret);
+            Console.WriteLine(avion.ModelAripi);
+            Console.WriteLine(avion.Marca);
+            Console.WriteLine(avion.TipAvion);
+            Console.WriteLine(avion.Model);
+            Console.WriteLine(avion.ModelTurbina);
+
+        }
+    }
+}
---
+++ teorieMostenire/Vehicul.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teorieMostenire
+{
+    public class Vehicul
+    {
+        //proprietati
+        private double pret;
+        private String marca;
+        private String model;
+
+        //constructori gol si unu de initialiare get si set
+       
+
+        public Vehicul(double pret, String marca, String model)
+        {
+            this.pret = pret;
+            this.marca = marca;
+            this.model = model;
+        }
+
+        public Vehicul(double pret)
+        {
+            Console.WriteLine("eu sunt constructorul cu un paramteru din baza");
+            this.pret = pret;
+            this.model = "undeifned";
+            this.marca = "undefined";
+
+        }
+
+
+
+        public double Pret
+        {
+            get { return pret; }
+            set {
+
+                if (value < 0)
+                {
+                    Console.WriteLine("nu vreau");
+                }
+                else
+                {
+                    pret = value;
+                }
+                      
+            
+            }
+        }
+
+        public String Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public String Model
+        {
+            get { return model; }
+            set { model= value; }
+        }
+       
+    }
+}
